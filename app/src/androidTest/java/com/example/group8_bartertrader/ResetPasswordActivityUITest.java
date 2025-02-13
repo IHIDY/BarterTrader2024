8a228f94e82e003df096093e7181bdb6a2455e92
Jaelyn Cox <jl630067@dal.ca>
2025-02-12 23:51:06 -0400

    Resolve US-5; ensure functionality and connect with login/settings pages

---
+++ app/src/androidTest/java/com/example/group8_bartertrader/ResetPasswordActivityUITest.java
@@ -0,0 +1,45 @@
+package com.example.group8_bartertrader;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ResetPasswordActivityUITest {
+    private static final int LAUNCH_TIMEOUT = 5000;
+    final String launcherPackage = "com.example.group8_bartertrader";
+    private UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackage);
+        assert launchIntent != null;
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(launchIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkIfLandingPageIsDisplayed() {
+//        updatePassword
+//                checkEmailExists
+    }
+}
