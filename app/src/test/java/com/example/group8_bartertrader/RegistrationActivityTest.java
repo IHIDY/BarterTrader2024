be3d2260276f4bab6e6360fb3ac1ba95d9a9d052
durling <al738398@dal.ca>
2025-02-15 12:54:25 -0400

    US-1: Updated JUnit tests in RegistrationActivityTest.java and logic in CredentialsValidator.java, added new tests aswell, pushing to branch alex

--- app/src/test/java/com/example/group8_bartertrader/RegistrationActivityTest.java
+++ app/src/test/java/com/example/group8_bartertrader/RegistrationActivityTest.java
@@ -1,10 +1,9 @@
 package com.example.group8_bartertrader;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class RegistrationActivityTest {
 
@@ -12,23 +11,30 @@ public class RegistrationActivityTest {
 
     @Before
     public void setup(){
+        System.setProperty("isUnitTest", "true");
         validator = new CredentialsValidator();
     }
 
     @Test
-    public void testFname(){
+    public void isFnameEmptyTest(){
         assertTrue(validator.isFnameEmpty(""));
         assertFalse(validator.isFnameEmpty("fName"));
     }
 
     @Test
-    public void testLname(){
+    public void isLnameEmptyTest(){
         assertTrue(validator.isLnameEmpty(""));
         assertFalse(validator.isLnameEmpty("lName"));
     }
 
     @Test
-    public void testEmail(){
+    public void isEmptyEmailTest(){
+        assertTrue(validator.isEmptyEmail("")); //empty
+        assertFalse(validator.isEmptyEmail("User@email.com")); //Valid email
+    }
+
+    @Test
+    public void isValidEmailTest(){
         assertFalse(validator.isValidEmail("")); //empty
         assertFalse(validator.isValidEmail("User@")); //missing the domain
         assertFalse(validator.isValidEmail("User@email")); //missing TLD (.com, .ca, etc)
@@ -37,7 +43,12 @@ public class RegistrationActivityTest {
     }
 
     @Test
-    public void testPassword(){
+    public void isEmptyPasswordTest(){
+        assertTrue(validator.isEmptyPass(""));//empty
+        assertFalse(validator.isEmptyPass("Password123!")); //Valid
+    }
+    @Test
+    public void isValidPasswordTest(){
         assertFalse(validator.isValidPass(""));//empty
         assertFalse(validator.isValidPass("pass"));//too short
         assertFalse(validator.isValidPass("password"));//missing numbers, etc...
@@ -45,9 +56,8 @@ public class RegistrationActivityTest {
         assertFalse(validator.isValidPass("Password123"));//missing special character
         assertTrue(validator.isValidPass("Password123!")); //Valid
     }
-
     @Test
-    public void testRole(){
+    public void isValidRoleTest(){
         assertFalse(validator.isValidRole("Select your role"));//invalid
         assertTrue(validator.isValidRole("Receiver"));//valid
         assertTrue(validator.isValidRole("Provider"));//valid
