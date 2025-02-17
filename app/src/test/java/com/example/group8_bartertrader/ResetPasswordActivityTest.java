90d5eea47bd23606b93bbe79128b4fb5dd5d19d6
zehua <zh885665@dal.ca>
2025-02-16 20:44:29 -0400

    US-5 complete

--- app/src/test/java/com/example/group8_bartertrader/ResetPasswordActivityTest.java
+++ app/src/test/java/com/example/group8_bartertrader/ResetPasswordActivityTest.java
@@ -1,27 +1,33 @@
 package com.example.group8_bartertrader;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class ResetPasswordActivityTest {
-
-    private CredentialsValidator cred;
+    CredentialsValidator validator;
 
     @Before
     public void setup(){
-        cred = new CredentialsValidator();
+        validator = new CredentialsValidator();
+    }
+
+    @Test
+    public void testEmail(){
+        assertFalse(validator.isValidEmail("")); //empty
+        assertFalse(validator.isValidEmail("User@")); //missing the domain
+        assertFalse(validator.isValidEmail("User@email")); //missing TLD (.com, .ca, etc)
+        assertFalse(validator.isValidEmail("user.email@com")); //wrong format
+        assertTrue(validator.isValidEmail("User@email.com")); //Valid
     }
 
     @Test
     public void testPassword(){
-        String noPass = "";
-        String tooShortPass = "123";
-        String validPass = "password123";
-
-        assertFalse(cred.isValidPass(noPass));
-        assertFalse(cred.isValidPass(tooShortPass));
-        assertTrue(cred.isValidPass(validPass));
+        assertFalse(validator.isValidPass(""));//empty
+        assertFalse(validator.isValidPass("pass"));//too short
+        assertFalse(validator.isValidPass("password"));//missing numbers, etc...
+        assertFalse(validator.isValidPass("password123"));//missing uppercase letter + special character
+        assertFalse(validator.isValidPass("Password123"));//missing special character
+        assertTrue(validator.isValidPass("Password123!")); //Valid
     }
-
 }
