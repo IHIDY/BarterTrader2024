b3425be47722e6cb9fc82b11f5e64aa1bd608fee
Ella Morais <el449918@dal.ca>
2025-03-12 14:52:19 -0300

    committing for fetch

--- app/src/androidTest/java/com/example/group8_bartertrader/DetailsActivityEspressoTest.java
+++ app/src/androidTest/java/com/example/group8_bartertrader/DetailsActivityEspressoTest.java
@@ -6,6 +6,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import static org.junit.Assert.assertTrue;
 import static java.util.regex.Pattern.matches;
 
 import android.content.Context;
@@ -73,7 +74,7 @@ public class DetailsActivityEspressoTest {
         device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
         UiObject exchangeActivity = device.findObject(new UiSelector().textContains("Get Current Location"));
 
-        assert exchangeActivity.exists();
+        assertTrue(exchangeActivity.exists());
     }
 
     @Test
@@ -84,6 +85,6 @@ public class DetailsActivityEspressoTest {
         device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
         UiObject productActivity = device.findObject(new UiSelector().resourceId("com.example.group8_bartertrader:id/receiverDashActivity"));
 
-        assert productActivity.exists();
+        assertTrue(productActivity.exists());
     }
 }
