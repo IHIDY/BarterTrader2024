cde099dc813fbfefe6ba6dcc9a654e097beb0ae4
Daniel Essien <dn541467@dal.ca>
2025-03-16 10:55:16 -0300

    Fix for ella

--- app/src/androidTest/java/com/example/group8_bartertrader/ReceiverDashUITest.java
+++ app/src/androidTest/java/com/example/group8_bartertrader/ReceiverDashUITest.java
@@ -48,7 +48,7 @@ public class ReceiverDashUITest {
                 allowButton.click();
             }
 
-            Espresso.onView(ViewMatchers.withId(R.id.title))
+            Espresso.onView(ViewMatchers.withId(R.id.textView2))
                     .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
                     .check(ViewAssertions.matches(ViewMatchers.withText("Receiver Dashboard")));
         }
