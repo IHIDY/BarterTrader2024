202d8426040fd458d97b3c5dd0367046f6e21d91
durling <al738398@dal.ca>
2025-04-01 16:16:10 -0300

    US-12: Fixed chatDisabled/chatEnabled Tests, and added a new test to check if a message has been sent in chat.

--- app/src/androidTest/java/com/example/group8_bartertrader/chatUITest.java
+++ app/src/androidTest/java/com/example/group8_bartertrader/chatUITest.java
@@ -1,18 +1,29 @@
 package com.example.group8_bartertrader;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 
+import android.app.Application;
 import android.content.Intent;
+import android.text.method.Touch;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.ViewAction;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -22,13 +33,8 @@ public class chatUITest {
     //When the offer has not been accepted yet, the chat button should not be visable
     @Test
     public void chatDisabledTest(){
-        String testOfferId = "TestNotAccepted";
-
-        //set offerAccepted to false before the activity launches
-        FirebaseDatabase.getInstance().getReference("chats").child(testOfferId).child("offerAccepted").setValue(false);
-
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ChatActivity.class);
-        intent.putExtra("offerId", testOfferId);
+        intent.putExtra("offerId", "-OMmjlFGgNTbk4Po5wKl"); //OfferId is pending (defaults to false)
         ActivityScenario<ChatActivity> scenario = ActivityScenario.launch(intent);
 
         onView(withId(R.id.sendButton)).check(matches(not(isEnabled())));
@@ -38,16 +44,23 @@ public class chatUITest {
     //When the offer is accepted, the chat button should appear
     @Test
     public void chatEnabledTest(){
-        String testOfferId = "TestAccepted";
-
-        //set offerAccepted to true before the activity launches
-        FirebaseDatabase.getInstance().getReference("chats").child(testOfferId).child("offerAccepted").setValue(true);
-
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ChatActivity.class);
-        intent.putExtra("offerId", testOfferId);
+        intent.putExtra("offerId", "-OMX3e7jHiipVy4FnViz"); //OfferId is accepted (true)
         ActivityScenario<ChatActivity> scenario = ActivityScenario.launch(intent);
 
         onView(withId(R.id.sendButton)).check(matches(isEnabled()));
         onView(withId(R.id.messageEditText)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void sendMessageTest(){
+        String testMessage = "Test Message sent";
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ChatActivity.class);
+        intent.putExtra("offerId", "-OMX3e7jHiipVy4FnViz");
+        ActivityScenario<ChatActivity> scenario = ActivityScenario.launch(intent);
+
+        onView(withId(R.id.messageEditText)).perform(typeText(testMessage));
+        onView(withId(R.id.sendButton)).perform(click());
+        onView(withText(testMessage)).check(matches(isDisplayed()));
     }
 }
